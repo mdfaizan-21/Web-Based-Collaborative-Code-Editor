6bd08b102d6644585e872b96d263c2ec46ccced0
Parul Yadav <2k23.cs2312483@gmail.com>
2025-10-14 12:18:18 +0530

    Started Collaborating

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen bg-background">test</div>
+    <div className="flex flex-col w-screen h-screen bg-background">started collaborating</div>
   );
 }
