49bf133f1e21e66642329ab96f66319ecde5cdbd
Faizan <2k23.cs2314006@gmail.com>
2026-04-18 14:44:36 +0530

    Restructured project layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,9 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import Image from "next/image";
 
-export default async function Home() {
-  const user = await currentUser();
-
-  if (user) {
-    redirect("/dashboard");
-  }
-
+export default function Home() {
   return (
-    <div className="flex overflow-hidden overscroll-none flex-col w-screen h-screen bg-background">
-      <div className="w-full max-w-screen-md px-8 flex flex-col items-center mx-auto">
-        
-        <h1 className="text-2xl font-medium text-center mt-32">
-          A Collaborative Cloud Code Editor, AI Powered, Auto-Scaling Copilot
-        </h1>
-
-        <div className="text-muted-foreground mt-4 text-center">
-          Collaborative Cloud Code Editor is a virtual box code editing environment with custom
-          AI code autocompletion and real-time collaboration. The infrastructure runs on 
-          Docker containers and Kubernetes to scale automatically based on resource consumption.
-        </div>
-
-        <div className="mt-8 flex space-x-4">
-          <Link href="/sign-up">
-            <Button>Go To App</Button>
-          </Link>
-        </div>
-
-        <div className="w-full rounded-lg bg-neutral-800 mt-12 aspect-video"></div>
-
-      </div>
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-9xl font-bold text-white">Vibe Code Editor</h1>
     </div>
   );
 }
