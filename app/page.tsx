8fe58f371f94effbb6ea9796a84d229ecf213f60
Faizan <2k23.cs2314006@gmail.com>
2026-05-03 19:52:15 +0530

    Added necessary components in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-9xl font-bold text-white">Vibe Code Editor</h1>
+      <h1 className="text-9xl font-bold text-white underline decoration-purple-500 decoration-8">
+        Vibe Code Editor
+      </h1>
     </div>
   );
 }
